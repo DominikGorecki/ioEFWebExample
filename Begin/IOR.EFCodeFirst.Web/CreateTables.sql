a8dcd962e6719587a712f1d360cb14e4bd70b3fd
Dominik G <dom@nikgo.com>
2020-03-06 15:02:50 -0500

    First Commit

---
+++ Begin/IOR.EFCodeFirst.Web/CreateTables.sql
@@ -0,0 +1,42 @@
+USE QuizesExample;
+
+CREATE TABLE Quizes 
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Title VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE Questions
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	QuestionOrder INT NOT NULL,
+	Question VARCHAR(100) NOT NULL,
+	
+	-- One to many relationship with Quizes
+	QuizId INT FOREIGN KEY REFERENCES Quizes(Id)
+);
+
+CREATE TABLE Answers
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Question VARCHAR(255) NOT NULL,
+	IsCorrect BIT NOT NULL,
+
+	-- One to many relationship with Questions
+	QuestionId INT FOREIGN KEY REFERENCES Questions(Id)
+);
+
+CREATE TABLE Tag
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL
+);
+
+-- Many to many relationship with Quizes
+CREATE TABLE Quizes_Tags
+(
+	QuizId INT NOT NULL FOREIGN KEY REFERENCES Quizes(Id),
+	TagId INT NOT NULL FOREIGN KEY REFERENCES Answers(Id),
+	PRIMARY KEY (QuizId, TagId)
+);

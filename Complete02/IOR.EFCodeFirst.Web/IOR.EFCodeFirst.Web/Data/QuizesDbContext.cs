2b2f4f32e9b64cd39dcf87a5dc93fa1a89b0abac
Dominik G <dom@nikgo.com>
2020-03-06 15:52:01 -0500

    Setup for Part 2 of recipe

---
+++ Complete02/IOR.EFCodeFirst.Web/IOR.EFCodeFirst.Web/Data/QuizesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOR.EFCodeFirst.Web.Data
+{
+    public class QuizesDbContext : DbContext
+    {
+        public QuizesDbContext(DbContextOptions options)  : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // We'll add code here
+        }
+    }
+}

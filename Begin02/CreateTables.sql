682ddb81acc1850716df3846070721bec2523e36
Dominik Gorecki <dgraphics519@gmail.com>
2020-03-07 16:23:07 -0500

    Reorganizing

---
+++ Begin02/CreateTables.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE QuizesExample;
+
+USE QuizesExample;
+
+CREATE TABLE Quizes 
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Title VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Questions
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	QuestionOrder INT NOT NULL,
+	Question VARCHAR(100) NOT NULL,
+	
+	-- One to many relationship with Quizes
+	QuizId INT FOREIGN KEY REFERENCES Quizes(Id)
+);
+
+CREATE TABLE Answers
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Answer VARCHAR(255) NOT NULL,
+	IsCorrect BIT NOT NULL,
+
+	-- One to many relationship with Questions
+	QuestionId INT FOREIGN KEY REFERENCES Questions(Id)
+);
+
+CREATE TABLE Tag
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL
+);
+
+-- Many to many relationship with Quizes
+CREATE TABLE Quizes_Tags
+(
+	QuizId INT NOT NULL FOREIGN KEY REFERENCES Quizes(Id),
+	TagId INT NOT NULL FOREIGN KEY REFERENCES Answers(Id),
+	PRIMARY KEY (QuizId, TagId)
+);
+
+DROP TABLE Quizes_Tags;
+DROP TABLE Tag;
+DROP TABLE Answers;

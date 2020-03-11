33453781e96412afd4f6e3e40fb8251defef1d80
Dominik G <dom@nikgo.com>
2020-03-11 14:52:51 -0400

    Small fix to previous check in

--- 03Complete/IOR.EFCodeFirst.Web/IOR.EFCodeFirst.Web/Data/QuizesDbContextConfig.cs
+++ 03Complete/IOR.EFCodeFirst.Web/IOR.EFCodeFirst.Web/Data/QuizesDbContextConfig.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IOR.EFCodeFirst.Web.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;

7e18446614e43c64afda902ba233e4304748bd97
Dominik G <dom@nikgo.com>
2020-03-11 14:21:40 -0400

    Reorganizing Repo

---
+++ 03Complete/IOR.EFCodeFirst.Web/IOR.EFCodeFirst.Web/Controllers/QuizesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IOR.EFCodeFirst.Web.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IOR.EFCodeFirst.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuizesController : ControllerBase
+    {
+        private readonly QuizesDbContext _dbContext;
+
+        public QuizesController(QuizesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetQuizes()
+        {
+            var quizes = await _dbContext.Quizes.ToListAsync();
+            return Ok(quizes);
+        }
+    }
+}
---
+++ 03Complete/IOR.EFCodeFirst.Web/IOR.EFCodeFirst.Web/Data/Models/QuizesTagsEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOR.EFCodeFirst.Web.Data.Models
+{
+    public class QuizesTagsEntity
+    {
+        public int QuizId { get; set; }
+        public int TagId { get; set; }
+    }
+}
